8052d73ac611ba4c18d108d61dfa9de3841e70bb
Rishit Dagli <rishit.dagli@gmail.com>
2021-04-04 09:31:58 +0530

    Update page

--- app.py
+++ app.py
@@ -21,11 +21,22 @@ def load_model():
     model = tf.keras.models.load_model('./models/plant_disease.h5')
     return model, class_indices
 
+# Display header.
+st.markdown("<br>", unsafe_allow_html=True)
+st.image(SEEDLING_EMOJI_URL, width=80)
 
-st.title("Plant Disease Recognizer🌱")
-st.subheader("A minimalistic web app to identify disease in plant leaves from images")
+"""
+# Plant Disease Recognizer
+[![Twitter](https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Fgithub.com%2FRishit-dagli%2FSurpriseHacks)](https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fgithub.com%2FRishit-dagli%2FSurpriseHacks)
+&nbsp[![GitHub Repo stars](https://img.shields.io/github/stars/Rishit-dagli/SurpriseHacks?style=social)](https://github.com/Rishit-dagli/SurpriseHacks/stargazers)
+&nbsp[![GitHub followers](https://img.shields.io/github/followers/Rishit-dagli?label=Follow&style=social)](https://github.com/Rishit-dagli)
+&nbsp[![Twitter Follow](https://img.shields.io/twitter/follow/rishit_dagli?style=social)](https://twitter.com/intent/follow?screen_name=rishit_dagli)
+"""
+st.markdown("<br>", unsafe_allow_html=True)
+"""A minimalistic web app to identify disease in plant leaves from images
+---
+"""
 
-st.text("")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
 
 if file is None:
